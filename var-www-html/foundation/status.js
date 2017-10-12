e8ccb99c2605294a34b58e0267fb180599e7767f
VK <kedia.vikas@gmail.com>
2017-10-11 20:42:49 -0700

    if metamask is not detected connecting to infura network

--- var-www-html/foundation/status.js
+++ var-www-html/foundation/status.js
@@ -24,7 +24,7 @@ jQuery(document).ready(function($) {
         } else {
             console.log('No web3? You should consider trying MetaMask!')
             // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-            web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+            web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/hvBxnkcxO7nF9ptQPkjz:8545"));
         }
         
         // Now you can start your app & access web3 freely:
