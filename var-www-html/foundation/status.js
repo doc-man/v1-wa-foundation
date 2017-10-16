26a010b7e98917815a8e276611e3c2d591340eec
mithun-grmtech <mithun@grmtech.com>
2017-10-16 16:57:04 +0530

    MC

--- var-www-html/foundation/status.js
+++ var-www-html/foundation/status.js
@@ -118,49 +118,6 @@ function createProposalsTableRow(pContractInstance, numberOfProposals) {
     }
 }
 var eathAccountsAddress = null;
-function getTransactionsByAccount(myaccount, startBlockNumber, endBlockNumber) {
-    console.log('Get Transactions By Account');
-  if (endBlockNumber == null) {
-    endBlockNumber = web3.eth.blockNumber;
-    console.log("Using endBlockNumber: " + endBlockNumber);
-  }
-  if (startBlockNumber == null) {
-    startBlockNumber = endBlockNumber - 1000;
-    console.log("Using startBlockNumber: " + startBlockNumber);
-  }
-  console.log("Searching for transactions to/from account \"" + myaccount + "\" within blocks "  + startBlockNumber + " and " + endBlockNumber);
-
-  for (var i = startBlockNumber; i <= endBlockNumber; i++) {
-    if (i % 1000 == 0) {
-      console.log("Searching block " + i);
-    }
-    web3.eth.getBlock(i, true, function(block){
-            
-        console.log(block);
-    });
-    // var block = web3.eth.getBlock(i, true);
-    // console.log(block);
-    // if (block != null && block.transactions != null) {
-    //   block.transactions.forEach( function(e) {
-    //     if (myaccount == "*" || myaccount == e.from || myaccount == e.to) {
-    //       console.log("  tx hash          : " + e.hash + "\n"
-    //         + "   nonce           : " + e.nonce + "\n"
-    //         + "   blockHash       : " + e.blockHash + "\n"
-    //         + "   blockNumber     : " + e.blockNumber + "\n"
-    //         + "   transactionIndex: " + e.transactionIndex + "\n"
-    //         + "   from            : " + e.from + "\n" 
-    //         + "   to              : " + e.to + "\n"
-    //         + "   value           : " + e.value + "\n"
-    //         + "   time            : " + block.timestamp + " " + new Date(block.timestamp * 1000).toGMTString() + "\n"
-    //         + "   gasPrice        : " + e.gasPrice + "\n"
-    //         + "   gas             : " + e.gas + "\n"
-    //         + "   input           : " + e.input);
-    //     }
-    //   })
-    // }
-  }
-  return true;
-}
 function startApp(){
 
     let tokenContract;
@@ -187,7 +144,6 @@ function startApp(){
             if(!error){
                 var accounts = web3.eth.accounts;
                 eathAccountsAddress = accounts[0];
-                console.log(getTransactionsByAccount(foundation, 1432400, 1432400));
                 if(eathAccountsAddress){
                     $('#showAdress').text(eathAccountsAddress);
                 } else {
@@ -496,7 +452,6 @@ jQuery(document).ready(function($) {
             alert('Please install metamask to interact with the contract.');
             return false;
         }
-        console.log("called create new proposal");
         var x = document.getElementById("proposalFormDiv");
         x.style.display = "block";
         var pBtn = document.getElementById("createNewProposal");
@@ -536,7 +491,6 @@ jQuery(document).ready(function($) {
                 beneficiary, amount, description,
                 function(error, result){
                     if(!error){
-                        console.log("Proposal tx: ",result);
                         var x = document.getElementById("proposalFormDiv");
                         x.style.display = "none";
                         var pBtn = document.getElementById("createNewProposal");
